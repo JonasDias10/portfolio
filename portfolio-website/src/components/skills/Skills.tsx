afb5dd22212557b6f633b94952079b8b8a7e57f5
Jonas Dias <zdias1413@gmail.com>
2024-02-14 22:05:22 -0300

    feat: add tailwind skill

--- portfolio-website/src/components/skills/Skills.tsx
+++ portfolio-website/src/components/skills/Skills.tsx
@@ -39,18 +39,22 @@ export const Skills = () => {
         {
             name: "Flutter",
             icon: "ri:flutter-fill"
+        },
+        {
+            name: "Tailwind",
+            icon: "teenyicons:tailwind-solid"
         }
     ]
 
     return (
-        <div className="max-w-7xl">
+        <div className="max-w-7xl" id="skills">
             <h1 className="text-center mb-10 text-4xl">
                 My 
                 <span className="font-bold ml-2">
                     Skills
                 </span>
             </h1>
-            <div className="max-w-7xl grid grid-cols-2 gap-2 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:gap-6">
+            <div className="grid grid-cols-2 gap-2 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:gap-6">
                 {skills.map(skill => (
                     <Skill 
                         key={skill.name} 
