976635a88d4bd7c4599fd1df1bbeccf458d64735
Jonas Dias <zdias1413@gmail.com>
2024-02-15 22:54:59 -0300

    feat: change width and height of skill box

--- portfolio-website/src/components/skill/Skill.tsx
+++ portfolio-website/src/components/skill/Skill.tsx
@@ -3,7 +3,7 @@ import { SkillType } from "types/appTypes"
 
 export const Skill = (skill: SkillType) => {
     return (
-        <div className="rounded-md border-2 border-black w-36 h-36 flex flex-col items-center justify-center hover:bg-black cursor-pointer hover:text-white sm:w-48 sm:h-48">
+        <div className="rounded-md border-2 border-black w-36 h-36 flex flex-col items-center justify-center hover:bg-black cursor-pointer hover:text-white sm:w-44 sm:h-44">
             <Icon icon={skill.icon} width="30px" />
             <p>
                 {skill.name}
