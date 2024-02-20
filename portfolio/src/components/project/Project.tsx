ba86d65e09ffafa2f4f42ae0f61982239a848bb1
Jonas Dias <zdias1413@gmail.com>
2024-02-20 00:06:37 -0300

    feat: add project component

---
+++ portfolio/src/components/project/Project.tsx
@@ -0,0 +1,25 @@
+import { Icon } from "@iconify/react/dist/iconify.js"
+import { ProjectType } from "types/appTypes"
+
+export const Project = (project: ProjectType) => {
+    const projectSide = project.index % 2 === 0 ? "flex-row":"flex-row-reverse"
+
+    return (
+        <div className={`flex flex-wrap gap-10 items-center justify-center max-w-4xl ${projectSide}`}>
+            <div className="rounded-md max-w-md max-h-96">
+                {project.image}
+            </div>
+            <div className="text-white max-w-80">
+                <h1 className="font-bold text-xl">
+                    {project.title}
+                </h1>
+                <p className="text-zinc-500 my-4 text-justify">
+                    {project.description}
+                </p>
+                <a href={project.url} target="_blank">
+                    <Icon icon="eva:external-link-outline" width="26px" />
+                </a>
+            </div>
+        </div>
+    )
+}
