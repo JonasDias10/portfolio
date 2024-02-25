321cff51982353676c6b60368061bb382e56ed88
Jonas Dias <zdias1413@gmail.com>
2024-02-25 01:19:13 -0300

    feat: add translation to about component

--- portfolio/src/components/about/About.tsx
+++ portfolio/src/components/about/About.tsx
@@ -1,27 +1,30 @@
 import AboutImage from "@assets/AboutImage.svg"
+import { useTranslation } from "react-i18next"
 
 export const About = () => {
+    const { t } = useTranslation()
+
     return (
-        <div className="flex flex-wrap gap-10 justify-center w-full px-4 lg:justify-between lg:px-0" id="about">
+        <section className="flex flex-wrap gap-10 justify-center w-full px-4 lg:justify-between lg:px-0" id="about">
             <div className="max-w-sm">
                 <img src={AboutImage} alt="About image." className="w-full" />
             </div>
             <div className="max-w-lg">
                 <h1 className="text-4xl mb-6">
-                    About <span className="font-bold">Me</span>
+                    {t("titles.about")} <span className="font-bold">{t("titles.me")}</span>
                 </h1>
                 <div className="flex flex-col gap-5 text-justify">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem ipsum dolorem totam quos. Unde dignissimos praesentium corporis. Reprehenderit dolor pariatur temporibus perferendis repellendus, commodi voluptatibus voluptate tempore enim error tenetur?
+                        {t("about.description1")}
                     </p>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, reiciendis quae. Minus nesciunt nisi expedita eius. Odit debitis doloremque enim illum eligendi dolor cumque veniam deleniti minus? Beatae, cum tempore.
+                        {t("about.description2")}
                     </p>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, reiciendis quae. Minus nesciunt nisi expedita eius. Odit debitis doloremque enim illum eligendi dolor cumque veniam deleniti minus? Beatae, cum tempore.
+                        {t("about.description3")}
                     </p>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
