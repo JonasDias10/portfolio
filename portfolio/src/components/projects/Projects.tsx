505af3257b79fa80e6243d7ceffa911731a458d9
Jonas Dias <zdias1413@gmail.com>
2024-02-26 00:17:56 -0300

    feat: add translation to projects component

--- portfolio/src/components/projects/Projects.tsx
+++ portfolio/src/components/projects/Projects.tsx
@@ -1,38 +1,51 @@
 import { Project } from "@components/project/Project"
 import { ProjectType } from "types/appTypes"
-import ProjectImage from "@assets/ProjectImage.svg"
+import { useTranslation } from "react-i18next"
+import NoteCards from "@assets/NoteCards.jpg"
+import TechStore from "@assets/TechStore.jpg"
+import WebChat from "@assets/WebChat.jpg"
+import LinkAggregator from "@assets/LinkAggregator.jpg"
 
 export const Projects = () => {
+    const { t } = useTranslation()
+
     const projects: ProjectType[] = [
         {
-            title: "Crypto Screener Application",
-            description: "I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.",
-            url: "https://github.com/JonasDias10/tech-store",
+            title: t("projects.note_cards.title"),
+            description: t("projects.note_cards.description"),
+            url: "https://github.com/JonasDias10/note-cards",
             index: 1,
-            image: <img src={ProjectImage} alt="Project image." />            
+            image: NoteCards
         },
         {
-            title: "Crypto Screener Application",
-            description: "I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.",
+            title: t("projects.tech_store.title"),
+            description: t("projects.tech_store.description"),
             url: "https://github.com/JonasDias10/tech-store",
             index: 2,
-            image: <img src={ProjectImage} alt="Project image." />            
+            image: TechStore
         },
         {
-            title: "Crypto Screener Application",
-            description: "I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.",
+            title: t("projects.web_chat.title"),
+            description: t("projects.web_chat.description"),
             url: "https://github.com/JonasDias10/tech-store",
             index: 3,
-            image: <img src={ProjectImage} alt="Project image." />            
+            image: WebChat      
+        },
+        {
+            title: t("projects.link_aggregator.title"),
+            description: t("projects.link_aggregator.description"),
+            url: "https://github.com/JonasDias10/tech-store",
+            index: 4,
+            image: LinkAggregator         
         }
     ]
 
     return (
-        <div className="bg-black w-full" id="projects">
-            <h1 className="text-center text-white mb-10 text-4xl my-12">
-                My 
+        <section className="bg-black w-full" id="projects">
+            <h1 className="text-center text-white mb-10 text-3xl my-12">
+                {t("titles.my1")}
                 <span className="font-bold ml-2">
-                    Projects
+                    {t("titles.projects")}
                 </span>
             </h1>
             <div className="flex gap-10 flex-col p-8">
@@ -49,6 +62,6 @@ export const Projects = () => {
                     ))
                 }
             </div>
-        </div>
+        </section>
     )
 }
