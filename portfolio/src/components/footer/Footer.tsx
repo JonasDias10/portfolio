f34e5d4dbde4a75a4c681a009ba7cc2251edf5c1
Jonas Dias <zdias1413@gmail.com>
2024-03-01 00:10:14 -0300

    feat: add credits

--- portfolio/src/components/footer/Footer.tsx
+++ portfolio/src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import { AppLogo } from "@assets/AppLogo"
 
 export const Footer = () => {
-    const currentYear = new Date().getFullYear()
+    const linkStyle = "font-bold hover:underline"
 
     return (
         <footer className="bg-black w-full flex items-center justify-between px-4 py-6">
@@ -10,12 +10,24 @@ export const Footer = () => {
                 <h1 className="font-bold">Portfolio</h1>
             </div>
             <div className="text-white text-right text-sm">
-                <h1>
-                    {currentYear} &copy; 
-                </h1>
-                <h2>
-                    Created by Jonas Dias
-                </h2>
+                <p>
+                    Developed by <a 
+                        href="https://github.com/JonasDias10" 
+                        target="_blank" 
+                        className={linkStyle}
+                    >
+                        Jonas Dias
+                    </a>
+                </p>
+                <p>
+                    Designed by <a 
+                        href="https://www.behance.net/jhanvishah2" 
+                        target="_blank"
+                        className={linkStyle}
+                    >
+                        Jhanvi Shah
+                    </a>
+                </p>
             </div>
         </footer>
     )
