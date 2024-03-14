fb846861874e976b7b8179794c81870aca28a87d
Jonas Dias <zdias1413@gmail.com>
2024-03-14 09:43:04 -0300

    style: change code formatting

--- portfolio/src/components/switchLanguage/SwitchLanguage.tsx
+++ portfolio/src/components/switchLanguage/SwitchLanguage.tsx
@@ -1,28 +1,32 @@
-import { Switch } from "@nextui-org/react"
-import { useTranslation } from "react-i18next"
-import { SwitchLanguageType } from "types/appTypes"
+import { Switch } from "@nextui-org/react";
+import { useTranslation } from "react-i18next";
+import { SwitchLanguageType } from "types/appTypes";
 
-export const SwitchLanguage = ({ isSelected, setIsSelected }: SwitchLanguageType) => {
-    const { i18n } = useTranslation()
+export const SwitchLanguage = ({
+  isSelected,
+  setIsSelected,
+}: SwitchLanguageType) => {
+  const { i18n } = useTranslation();
 
-    const handleSwitchChange = () => {
-        setIsSelected(prev => !prev)
-        i18n.changeLanguage(isSelected ? 'pt-BR' : 'en')
-    }
-    
-    return (
-        <Switch
-            isSelected={isSelected}
-            onValueChange={handleSwitchChange}
-            startContent={<span>PT</span>}
-            endContent={<span>EN</span>}
-            classNames={{
-                base: "px-2",
-                wrapper: "bg-black w-16 h-5 rounded-sm group-data-[selected=true]:bg-black",
-                thumb: "group-data-[selected=true]:ml-11 w-3 h-3 rounded-sm",
-                startContent: "text-sm p-1 text-white",
-                endContent: "text-sm p-1 text-white",
-            }}
-        />
-    )
-}
+  const handleSwitchChange = () => {
+    setIsSelected((prev) => !prev);
+    i18n.changeLanguage(isSelected ? "pt-BR" : "en");
+  };
+
+  return (
+    <Switch
+      isSelected={isSelected}
+      onValueChange={handleSwitchChange}
+      startContent={<span>PT</span>}
+      endContent={<span>EN</span>}
+      classNames={{
+        base: "px-2",
+        wrapper:
+          "bg-black w-16 h-5 rounded-sm group-data-[selected=true]:bg-black",
+        thumb: "group-data-[selected=true]:ml-11 w-3 h-3 rounded-sm",
+        startContent: "text-sm p-1 text-white",
+        endContent: "text-sm p-1 text-white",
+      }}
+    />
+  );
+};
--- portfolio/src/hooks/useHeader.tsx
+++ portfolio/src/hooks/useHeader.tsx
@@ -1,13 +1,13 @@
-import { HeaderContext } from "@contexts/HeaderContext"
-import { useContext } from "react"
-import { HeaderType } from "types/appTypes"
+import { HeaderContext } from "@contexts/HeaderContext";
+import { useContext } from "react";
+import { HeaderType } from "types/appTypes";
 
 export const useHeader = (): HeaderType => {
-    const context = useContext(HeaderContext)
+  const context = useContext(HeaderContext);
 
-    if (!context) {
-        throw new Error("useHeader must be used within a HeaderProvider")
-    }
-    
-    return context
-}
+  if (!context) {
+    throw new Error("useHeader must be used within a HeaderProvider");
+  }
+
+  return context;
+};
