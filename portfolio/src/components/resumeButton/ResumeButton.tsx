898692c5ee6333996acb91e7b96a5eadbfd8b96b
Jonas Dias <zdias1413@gmail.com>
2024-03-14 10:24:39 -0300

    fix: resume link

--- portfolio/src/components/resumeButton/ResumeButton.tsx
+++ portfolio/src/components/resumeButton/ResumeButton.tsx
@@ -6,7 +6,7 @@ export const ResumeButton = () => {
   const { t } = useTranslation();
 
   return (
-    <a href="https://drive.usercontent.google.com/download?id=1GT4hvhe_nyF8l-bxZ80NgeCkq3CVSi33&export=download&authuser=0&confirm=t&uuid=d201cac8-a531-41e5-9b7f-5bf819e64fec&at=APZUnTWW1F5oY1kHezlrv4wwEKsU:1710419565848">
+    <a href="https://drive.usercontent.google.com/download?id=17Zbdo_-p_jM4V-kK5FnePhI0ApazUgI1&export=download&authuser=0&confirm=t&uuid=0a5d1024-602f-4bac-b9d7-4090d4bdb8ee&at=APZUnTWpm6kuqyDIvdQ5B7SAV9LX:1710422631231">
       <Button className="bg-black text-white font-semibold hover:text-black hover:bg-white border-2 border-black h-10 rounded-md">
         {t("buttons.resume")}
         <Icon icon="material-symbols:download" width="22px" />
