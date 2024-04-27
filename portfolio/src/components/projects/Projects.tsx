82c404c8f3f14adac2c86ed3a7f019cbb393fc25
Jonas Dias <zdias1413@gmail.com>
2024-04-26 21:17:08 -0300

    feat: add new projects and change images

--- portfolio/src/components/projects/Projects.tsx
+++ portfolio/src/components/projects/Projects.tsx
@@ -1,10 +1,8 @@
 import { Project } from "@components/project/Project";
 import { ProjectType } from "types/appTypes";
 import { useTranslation } from "react-i18next";
-import noteCards from "@assets/note-cards.jpg";
-import techStore from "@assets/tech-store.jpg";
-import webChat from "@assets/web-chat.jpg";
-import linkAggregator from "@assets/link-aggregator.jpg";
+import linkAggregator from "@assets/link-aggregator.png";
+import progressTrack from "@assets/progress-track.png";
 
 export const Projects = () => {
   const { t } = useTranslation();
@@ -15,21 +13,24 @@ export const Projects = () => {
       description: t("projects.note_cards.description"),
       url: "https://github.com/JonasDias10/note-cards",
       index: 1,
-      image: noteCards,
+      image:
+        "https://github.com/JonasDias10/note-cards/blob/main/media/note-cards-gif.gif?raw=true",
     },
     {
       title: t("projects.tech_store.title"),
       description: t("projects.tech_store.description"),
       url: "https://github.com/JonasDias10/tech-store",
       index: 2,
-      image: techStore,
+      image:
+        "https://github.com/JonasDias10/tech-store/blob/main/media/tech-store.gif?raw=true",
     },
     {
       title: t("projects.web_chat.title"),
       description: t("projects.web_chat.description"),
       url: "https://github.com/JonasDias10/web-chat",
       index: 3,
-      image: webChat,
+      image:
+        "https://github.com/JonasDias10/web-chat/blob/main/media/web-chat.gif?raw=true",
     },
     {
       title: t("projects.link_aggregator.title"),
@@ -37,6 +38,21 @@ export const Projects = () => {
       url: "https://jonas-dias.netlify.app",
       index: 4,
       image: linkAggregator,
+    },
+    {
+      title: t("projects.unit_tests.title"),
+      description: t("projects.unit_tests.description"),
+      url: "https://github.com/JonasDias10/unit-tests",
+      index: 5,
+      image:
+        "https://github.com/JonasDias10/unit-tests/blob/main/media/tests-results.png?raw=true",
+    },
+    {
+      title: t("projects.progress_track.title"),
+      description: t("projects.progress_track.description"),
+      url: "https://github.com/micaelomota/live-youtube-web-app",
+      index: 6,
+      image: progressTrack,
     },
   ];
 
