0eea5b638d0ae70e41a89c6c5dcb6b045209e85a
Jonas Dias <zdias1413@gmail.com>
2024-05-10 23:18:19 -0300

    fix: resume link

--- portfolio/src/components/resumeButton/ResumeButton.tsx
+++ portfolio/src/components/resumeButton/ResumeButton.tsx
@@ -6,7 +6,7 @@ export const ResumeButton = () => {
   const { t } = useTranslation();
 
   return (
-    <a href="https://drive.usercontent.google.com/download?id=17Zbdo_-p_jM4V-kK5FnePhI0ApazUgI1&export=download&authuser=0&confirm=t&uuid=0a5d1024-602f-4bac-b9d7-4090d4bdb8ee&at=APZUnTWpm6kuqyDIvdQ5B7SAV9LX:1710422631231">
+    <a href="https://jonas-dias-portfolio.netlify.app">
       <Button className="bg-black text-white font-semibold hover:text-black hover:bg-white border-2 border-black h-10 rounded-md">
         {t("buttons.resume")}
         <Icon icon="material-symbols:download" width="22px" />
