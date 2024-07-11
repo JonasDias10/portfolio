47f41224280229613bc317e103495889908c6524
Jonas Dias <zdias1413@gmail.com>
2024-07-11 11:21:56 -0300

    fix: tailwind config

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,19 +1,17 @@
-const { nextui } = require("@nextui-org/react");
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [
-    nextui({
-      addCommonColors: true,
-    })
-  ]
-}
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+];
+export const theme = {
+  extend: {},
+};
+export const darkMode = "class";
+export const plugins = [
+  nextui({
+    addCommonColors: true,
+  })
+];
